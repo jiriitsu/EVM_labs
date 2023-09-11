ad600f95ae8f5c85b18e1f8c44e5e277fee3f48e
jiriitsu <dkarpova2043@gmail.com>
2023-09-11 15:35:46 +0500

    Init repo

---
+++ Lab1C/Lab1Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Net.Mail;
+
+struct Message
+{
+    public bool Result {get;set;}
+    public int Data {get;set;}
+}
+class PipeClient
+{
+    static void Main(string[] args)
+    {
+        Message message = new Message();
+        message.Data = 10;
+        message.Result = false;
+    
+        using (var newClient = new NamedPipeClientStream("Pipe_lab1"))
+        {
+            byte[] bytes= new byte[Unsafe.SizeOf<Message>()];
+            Console.WriteLine("Client is working...");
+            newClient.Connect();
+            newClient.Read(bytes);
+            message = MemoryMarshal.Read<Message>(bytes);
+            message.Result = true;
+            MemoryMarshal.Write(bytes, ref message);
+            Console.WriteLine("{0}, {1}", message.Result, message.Data);
+            newClient.Write(bytes);
+            Console.WriteLine("{0}, {1}", message.Result, message.Data);
+
+        }
+        Console.WriteLine("Client's work is done");
+        Console.ReadLine();
+    }
+}   
---
+++ Lab1S/Lab1Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+struct Message
+{
+    public bool Result{get;set;}
+    public int Data {get;set;}
+}
+
+class PipeClient
+{
+    
+
+    static void Main(string[] args)
+    {
+        Message message = new Message();
+        message.Data = 10;
+        message.Result = false;
+        using (var newServer = new NamedPipeServerStream("Pipe_lab1"))
+        {
+                        
+            Console.WriteLine("Server is working...");
+            
+            //struct to byte
+            byte[] bytes= new byte[Unsafe.SizeOf<Message>()];
+            MemoryMarshal.Write(bytes, ref message);
+            Console.WriteLine("{0}, {1}", message.Data, message.Result);  
+            newServer.WaitForConnection();
+            newServer.Write(bytes);
+            newServer.Read(bytes);
+            message = MemoryMarshal.Read<Message>(bytes);
+            Console.WriteLine("{0}", message.Result);
+
+            
+        }
+        Console.WriteLine("Server's work is done");
+        Console.ReadLine();
+    }
+}
