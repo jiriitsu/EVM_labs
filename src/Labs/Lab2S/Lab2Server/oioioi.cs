9d266efac9763dc1b77e406ab0ee5851a30ef47e
Dina <107078656+jiriitsu@users.noreply.github.com>
2023-09-21 15:50:41 +0500

    Create oioioi.cs

---
+++ src/Labs/Lab2S/Lab2Server/oioioi.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Net.Mail;
+using System.Data;
+using Microsoft.VisualBasic;
+
+
+// Добавление данных для отправки в очередь должно происходить до тех пор
+// пока не будет нажата комбинация клавиш Ctrl+C. Также при добавлении данных в очередь должен учитываться приоритет отправляемых данных. 
+// Полученные обратно от второго консольного приложения (клиент) данные должны сохраняться в буфер, 
+// который будет записываться в файл или выводиться на экран после нажатия комбинации клавиш Ctrl+C.
+struct Message
+{
+    public string MyString {get;set;}
+    public int Priority {get;set;}
+}
+
+class PipeServer
+{
+    private static bool inputRunning = true;
+    private static PriorityQueue<Message,int> myQueue = new();
+
+    static void Main(string[] args)
+    {
+        //Message
+        Message message = new Message();
+        
+        //Init data
+        Console.WriteLine("Enter your data(CTRC+C to interrupt)");
+        Console.CancelKeyPress += delegate(object? sender, ConsoleCancelEventArgs e) {
+            e.Cancel = true;
+            inputRunning = false;
+        };     
+        while(inputRunning)
+        {
+            Console.WriteLine("Enter the first value");
+            //to message
+            Console.WriteLine("Enter the 2nd value");
+            //esho chota
+        
+            Console.WriteLine(message.MyString);
+            Console.WriteLine("Enter the priority of value");
+            if(int.TryParse(Console.ReadLine(), out int priority)){           
+                myQueue.Enqueue(message, priority);
+            }
+            else
+            {
+                myQueue.Enqueue(message,0);
+            }
+        }
+        myQueue.Dequeue 
+    
+    
+        using (var newServer = new NamedPipeClientStream("Pipe_lab2"))
+        {
+            // byte[] bytes= new byte[Unsafe.SizeOf<Message>()];
+            // Console.WriteLine("Client is working...");
+            // newClient.Connect();
+            // newClient.Read(bytes);
+            // message = MemoryMarshal.Read<Message>(bytes);
+            // message.Result = true;
+            // MemoryMarshal.Write(bytes, ref message);
+            // Console.WriteLine("{0}, {1}", message.Result, message.Data);
+            // newClient.Write(bytes);
+            // Console.WriteLine("{0}, {1}", message.Result, message.Data);
+
+        }
+        Console.WriteLine("Client's work is done");
+        Console.ReadLine();
+    }
+}   
