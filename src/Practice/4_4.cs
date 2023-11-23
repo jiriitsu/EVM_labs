75a3c5287388f31b2a5189ee6356feb66964f238
Dina <107078656+jiriitsu@users.noreply.github.com>
2023-11-23 15:51:42 +0500

    Update 4_4.cs

--- src/Practice/4_4.cs
+++ src/Practice/4_4.cs
@@ -5,19 +5,21 @@ using Iot.Device.OneWire;
 using Iot.Device.RotaryEncoder;
 using Raven.Iot.Device;
 using Raven.Iot.Device.GpioExpander;
+using Raven.Iot.Device.Ina219;
 using UnitsNet;
+
 
 var duration = TimeSpan.FromMinutes(1);
 var interval = TimeSpan.FromSeconds(10);
+
 if (DeviceHelper.GetGpioExpanderDevices() is [I2cConnectionSettings settings])
 {
     using Ina219 device = new Ina219(settings);
 
-    device.BusVoltageRange = Ina219BusVoltageRange.Range16v;
-    device.PgaSensitivity = Ina219PgaSensitivity.PlusOrMinus40mv;
-    device.SetCalibration(33574, (float)12.2e-6);
+    TimeProvider.CreateTimer()
+    List<(double Voltage, double Power, double Current)> data = new List<(double Voltage, double Power, double Current)>();
    
-    while(true)
+    while(/*timer*/)
     {
         Console.WriteLine($"Bus Voltage {device.ReadBusVoltage()} Power {device.ReadPower() * 1000}mW Current {device.ReadCurrent()}mA") ;
     }
